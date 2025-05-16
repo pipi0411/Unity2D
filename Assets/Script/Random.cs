d9b2ac09e387ed27b984f9d8abf592cb38e0d633
Pupa06 <20a41010217@students.hou.edu.vn>
2025-05-16 20:03:06 +0700

    update

---
+++ Assets/Script/Random.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+
+public class Random : MonoBehaviour
+{
+    [SerializeField] int width;
+    [SerializeField] GameObject dirt;
+    void Start()
+    {
+        Generation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Generation(){
+        for (int x = 0; x < width; x++)
+        {
+            Instantiate(dirt, new Vector2(x, 0), Quaternion.identity);
+        }
+    }
+}
